50a502c0b97b6ebae8233c2e745c73a75535389c
deokudev <sddev@gmail.com>
2023-01-17 08:06:30 +0900

    카테고리 항상 펼치기 기능 추가

---
+++ custom-script.js
@@ -0,0 +1,4 @@
+try {
+  expandTree();
+} catch (e) {}
+// 출처 : https://psychoria.tistory.com/82#:~:text=%EB%8B%A4%EC%9D%8C%EA%B3%BC%20%EA%B0%99%EC%9D%B4%20%23%23_category,%ED%95%98%EA%B2%8C%20%EC%B9%B4%ED%85%8C%EA%B3%A0%EB%A6%AC%EA%B0%80%20%ED%8E%BC%EC%B3%90%EC%A7%91%EB%8B%88%EB%8B%A4.
